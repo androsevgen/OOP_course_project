cd533ebf8d643a479a294c16ee80d1a023a7e1ba
andros.evgen <andros.evgen@gmail.com>
2016-10-18 22:22:09 +0300

    commit

--- src/project/Catalog.java
+++ src/project/Catalog.java
@@ -4,7 +4,7 @@ public class Catalog extends Parametric implements Inter1 {
     private String laptops;
     private String desktop_computer;
     private String smartphone;
-    private String computer_Parts;
+    private String Cameras; //фотоапарати
 
     @Override
     public void chooseaproduct() {
